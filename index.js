b5397d29374ce83a6db0c914bf6e896b5de50b44
Chalita05 <6306021620051@fitm.kmutnb.ac.th>
2022-04-05 19:50:57 +0700

    github

---
+++ index.js
@@ -0,0 +1,6 @@
+function findPerimeter(l, w) { //l = length,w = width)
+    return (l + w) * 2;
+}
+console.log(findPerimeter(6, 7));         
+console.log(findPerimeter(20, 10));
+console.log(findPerimeter(2, 9));
